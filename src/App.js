b5ae86aa15be3e438fad2ff03d14ccceab25d4b4
Leonardo Motta <leo@domboscoatacado.com.br>
2019-02-02 20:57:54 -0500

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import Layout from './hoc/Layout/Layout';
+import Login from './containers/Login/Login';
+import PageNotFound from './containers/PageNotFound/PageNotFound';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Layout>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Redirect exact from="/" to="/login" />
+
+            <Route exact path="/login/:id" />
+
+            <Route component={PageNotFound} />
+          </Switch>
+        </Layout>
       </div>
     );
   }
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const layout = props => {
+  return (
+    <>
+      <p>test</p>
+      <main>{props.children}</main>
+    </>
+  );
+};
+
+export default layout;
